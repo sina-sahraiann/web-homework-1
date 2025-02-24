1842101704f87a3057701e1903b41223341da8d6
sina-sahraiann <sinasahraeian780@gmail.com>
2025-02-24 23:48:12 +0330

    added

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    output: "export",
+    basePath: "/web-homework-1",
+    assetPrefix: "/web-homework-1/",
 };
 
 export default nextConfig;
